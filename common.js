94da02c766d09657b0c30550e89a47acbfb23587
Mykolas Krupauskas <m.krupauskas@gmail.com>
2017-06-11 21:28:38 +0300

    Imported final version
    this was the version we presented at the hackathon

--- common.js
+++ common.js
@@ -12,10 +12,10 @@ var authorizationBasic = (clientId + ':' + clientSecret);
 
 var request = new XMLHttpRequest();
 request.open('POST', oAuth.AuthorizationServer, true);
-request.setRequestHeader('Content-Type', 'https://www.emcom.eu/api/v1/device/3975/; charset=UTF-8');
+request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
 request.setRequestHeader('Authorization', 'Basic ' + authorizationBasic);
 request.setRequestHeader('Accept', 'application/json');
-request.send("username=Parcel_Condition&password=iot101&grant_type=password");
+request.send("username=John&password=Smith&grant_type=password");
 
 request.onreadystatechange = function () {
     if (request.readyState === 4) {
