3c4a67199dc0778c1fa0e2ffa8f8842bff9df950
Mykolas Krupauskas <m.krupauskas@gmail.com>
2017-08-10 20:58:08 +0300

    updated the whole project

--- SecondInterfaceVersion/script.js
+++ SecondInterfaceVersion/script.js
@@ -1,119 +1,123 @@
-let map;
-let data;
+let map,
+    data;
 let legend = document.getElementById('legend');
 const title = document.getElementById('title');
-let coordLat, coordLng, temperature, humidity, battery;
-let infoWindowContent, legendInfoContent;
-let latlng;
+let coordLat,
+    coordLng,
+    temperature,
+    humidity,
+    battery;
+let infoWindowContent,
+    legendInfoContent;
 
 function getData() {
-	$.ajax({
-		url: '../data.json',
-		async: false,
-		success: function(tempData) {
-			data = tempData;
-			coordLat = data['live']['2'];
-			coordLng = data['live']['3'];
-			temperature = data.live['200'];
-			humidity = Math.round(data.live['220']);
-			battery = data.live['10'];
-		},
-		dataType: 'json'
-	});
+    $.ajax({
+        url: '../data.json',
+        async: false,
+        success: function(tempData) {
+            data = tempData;
+            coordLat = data['live']['2'];
+            coordLng = data['live']['3'];
+            temperature = data.live['200'];
+            humidity = Math.round(data.live['220']);
+            battery = data.live['10'];
+        },
+        dataType: 'json'
+    });
 }
 
 function initMap() {
-	getData();
-	map = new google.maps.Map(document.getElementById('map'), {
-		zoom: 16,
-		mapTypeId: google.maps.MapTypeId.ROADMAP,
-		disableDefaultUI: true,
-		zoomControl: true,
-		center: new google.maps.LatLng(coordLat, coordLng),
-		styles: [
-			{ featureType: 'all', elementType: 'all', stylers: [{ hue: '#008eff' }] },
-			{
-				featureType: 'poi',
-				elementType: 'all',
-				stylers: [{ visibility: 'off' }]
-			},
-			{
-				featureType: 'road',
-				elementType: 'all',
-				stylers: [{ saturation: '0' }, { lightness: '0' }]
-			},
-			{
-				featureType: 'transit',
-				elementType: 'all',
-				stylers: [{ visibility: 'off' }]
-			},
-			{
-				featureType: 'water',
-				elementType: 'all',
-				stylers: [
-					{ visibility: 'simplified' },
-					{ saturation: '-60' },
-					{ lightness: '-20' }
-				]
-			}
-		]
-	});
+    getData();
+    map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 16,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        disableDefaultUI: true,
+        zoomControl: true,
+        center: new google.maps.LatLng(coordLat, coordLng),
+        styles: [
+            { featureType: 'all', elementType: 'all', stylers: [{ hue: '#008eff' }] },
+            {
+                featureType: 'poi',
+                elementType: 'all',
+                stylers: [{ visibility: 'off' }]
+            },
+            {
+                featureType: 'road',
+                elementType: 'all',
+                stylers: [{ saturation: '0' }, { lightness: '0' }]
+            },
+            {
+                featureType: 'transit',
+                elementType: 'all',
+                stylers: [{ visibility: 'off' }]
+            },
+            {
+                featureType: 'water',
+                elementType: 'all',
+                stylers: [
+                    { visibility: 'simplified' },
+                    { saturation: '-60' },
+                    { lightness: '-20' }
+                ]
+            }
+        ]
+    });
 
-	let marker = new google.maps.Marker({
-		position: new google.maps.LatLng(coordLat, coordLng),
-		icon: '../images/test.png',
-		map: map
-	});
+    let marker = new google.maps.Marker({
+        position: new google.maps.LatLng(coordLat, coordLng),
+        icon: '../images/test.png',
+        map: map
+    });
 
-	let infoWindow = new google.maps.InfoWindow();
-	marker.addListener('click', function(event) {
-		infoWindow.setContent(infoWindowContent);
-		infoWindow.setPosition(event.latLng);
-		infoWindow.open(map);
-	});
+    let infoWindow = new google.maps.InfoWindow();
+    marker.addListener('click', function(event) {
+        infoWindow.setContent(infoWindowContent);
+        infoWindow.setPosition(event.latLng);
+        infoWindow.open(map);
+    });
 
-	map.controls[google.maps.ControlPosition.TOP_CENTER].push(title);
-	map.controls[google.maps.ControlPosition.LEFT_CENTER].push(legend);
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(title);
+    map.controls[google.maps.ControlPosition.LEFT_CENTER].push(legend);
 
-	changeData();
-	setInterval(changeData, 2000);
+    changeData();
+    setInterval(changeData, 2000);
 
-	function changeMap() {
-		getData();
-		let latlng = new google.maps.LatLng(coordLat, coordLng);
-		marker.setPosition(latlng);
-	}
+    function changeMap() {
+        getData();
+        let latlng = new google.maps.LatLng(coordLat, coordLng);
+        marker.setPosition(latlng);
+    }
 
-	setInterval(changeMap, 2000);
+    setInterval(changeMap, 2000);
 }
 
 function changeData() {
-	getData();
+    getData();
 
-	infoWindowContent =
-		"<div id='infoBox'><p>latitude: " +
-		coordLat +
-		'<br/>longitude: ' +
-		coordLng +
-		'</p></div>';
-	legendInfoContent = [
-		'<p id="info">Information</p>',
-		'<p><b>Temperature: </b>' +
-			temperature +
-			'°C</br><span>(Optimal)</span></p>',
-		'<p><b>Humidity: </b>' + humidity + '%</br><span>(Optimal)</span></p>',
-		'<p ><b>Battery: </b>' + battery + '%</br><span>(Optimal)</span></p>',
-		'<p><b>Condition: </b><span>Good</span></p>'
-	];
+    infoWindowContent =
+        "<div id='infoBox'><p>latitude: " +
+        coordLat +
+        '<br/>longitude: ' +
+        coordLng +
+        '</p></div>';
+    legendInfoContent = [
+        '<p id="info">Information</p>',
+        '<p><b>Temperature: </b>' +
+        temperature +
+        '°C</br><span>(Optimal)</span></p>',
+        '<p><b>Humidity: </b>' + humidity + '%</br><span>(Optimal)</span></p>',
+        '<p ><b>Battery: </b>' + battery + '%</br><span>(Optimal)</span></p>',
+        '<p><b>Condition: </b><span>Good</span></p>'
+    ];
 
-	drawLegend();
+    drawLegend();
 }
 
 function drawLegend() {
-	legend.innerHTML = '';
-	for (let i = 0; i < legendInfoContent.length; i++) {
-		let holder = document.createElement('div');
-		holder.innerHTML = legendInfoContent[i];
-		legend.appendChild(holder);
-	}
+    legend.innerHTML = '';
+    for (let i = 0; i < legendInfoContent.length; i++) {
+        let holder = document.createElement('div');
+        holder.innerHTML = legendInfoContent[i];
+        legend.appendChild(holder);
+    }
 }
--- script.js
+++ script.js
@@ -1,68 +1,70 @@
-let mapProp;
-let map;
-let marker;
-let coordLat = 0;
-let coordLng = 0;
-let data;
+'use strict';
+
+let mapProp,
+    map,
+    marker,
+    data;
+let coordLat = 0,
+    coordLng = 0;
 
 function getData() {
-	$.ajax({
-		url: 'data.json',
-		async: false,
-		success: function(tempData) {
-			data = tempData;
-			coordLat = data.live['2'];
-			coordLng = data.live['3'];
-		},
-		dataType: 'json'
-	});
+    $.ajax({
+        url: 'data.json',
+        async: false,
+        success: function(tempData) {
+            data = tempData;
+            coordLat = data.live['2'];
+            coordLng = data.live['3'];
+        },
+        dataType: 'json'
+    });
 }
 
 function initMap() {
-	getData();
-	mapProp = {
-		center: new google.maps.LatLng(coordLat, coordLng),
-		zoom: 16,
-		styles: [
-			{ featureType: 'all', elementType: 'all', stylers: [{ hue: '#008eff' }] },
-			{
-				featureType: 'poi',
-				elementType: 'all',
-				stylers: [{ visibility: 'off' }]
-			},
-			{
-				featureType: 'road',
-				elementType: 'all',
-				stylers: [{ saturation: '0' }, { lightness: '0' }]
-			},
-			{
-				featureType: 'transit',
-				elementType: 'all',
-				stylers: [{ visibility: 'off' }]
-			},
-			{
-				featureType: 'water',
-				elementType: 'all',
-				stylers: [
-					{ visibility: 'simplified' },
-					{ saturation: '-60' },
-					{ lightness: '-20' }
-				]
-			}
-		]
-	};
-	map = new google.maps.Map(document.getElementById('map'), mapProp);
-	marker = new google.maps.Marker({
-		position: mapProp.center,
-		icon: 'images/test.png'
-	});
-	marker.setMap(map);
-	setInterval(changeData, 2000);
-	map.panTo(new google.maps.LatLng(coordLat, coordLng));
+    getData();
+    mapProp = {
+        center: new google.maps.LatLng(coordLat, coordLng),
+        zoom: 16,
+        styles: [
+            { featureType: 'all', elementType: 'all', stylers: [{ hue: '#008eff' }] },
+            {
+                featureType: 'poi',
+                elementType: 'all',
+                stylers: [{ visibility: 'off' }]
+            },
+            {
+                featureType: 'road',
+                elementType: 'all',
+                stylers: [{ saturation: '0' }, { lightness: '0' }]
+            },
+            {
+                featureType: 'transit',
+                elementType: 'all',
+                stylers: [{ visibility: 'off' }]
+            },
+            {
+                featureType: 'water',
+                elementType: 'all',
+                stylers: [
+                    { visibility: 'simplified' },
+                    { saturation: '-60' },
+                    { lightness: '-20' }
+                ]
+            }
+        ]
+    };
+    map = new google.maps.Map(document.getElementById('map'), mapProp);
+    marker = new google.maps.Marker({
+        position: mapProp.center,
+        icon: 'images/test.png'
+    });
+    marker.setMap(map);
+    setInterval(changeData, 2000);
+    map.panTo(new google.maps.LatLng(coordLat, coordLng));
 }
 
 function changeData() {
-	getData();
-	let latlng = new google.maps.LatLng(coordLat, coordLng);
-	marker.setPosition(latlng);
+    getData();
+    let latlng = new google.maps.LatLng(coordLat, coordLng);
+    marker.setPosition(latlng);
 }
